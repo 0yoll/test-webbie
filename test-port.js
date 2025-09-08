ed507d05bf77536e53d416c2d9916123a71a5412
Miguel Luíz <summeryseasony@gmail.com>
2025-09-07 22:48:27 -0300

    Add files via upload

---
+++ test-port.js
@@ -0,0 +1,20 @@
+// ===== Destacar seção ativa no menu =====
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
